f723e3ef29d349681d1b9874423fc87d3fb8ee07
Marc Leeman <m.leeman@televic.com>
2024-04-30 11:20:31 +0200

    New upstream version 1.24.3

--- sys/va/gstvabaseenc.c
+++ sys/va/gstvabaseenc.c
@@ -478,12 +478,9 @@ _push_buffer_to_downstream (GstVaBaseEnc * base, GstVideoCodecFrame * frame)
   if (complete) {
     ret = gst_video_encoder_finish_frame (GST_VIDEO_ENCODER (base), frame);
   } else {
-    if (frame->output_buffer) {
-      ret = gst_video_encoder_finish_subframe (GST_VIDEO_ENCODER (base), frame);
-    } else {
-      /* Allow to output later and no data here. */
-      ret = GST_FLOW_OK;
-    }
+    /* Allow to output later and no data here. */
+    g_assert (!frame->output_buffer);
+    ret = GST_FLOW_OK;
   }
 
   return ret;
@@ -640,8 +637,10 @@ gst_va_base_enc_handle_frame (GstVideoEncoder * venc,
       GST_TIME_ARGS (GST_BUFFER_PTS (frame->input_buffer)));
 
   if (g_atomic_int_compare_and_exchange (&base->reconf, TRUE, FALSE)) {
-    if (!gst_va_base_enc_reset (base))
+    if (!gst_va_base_enc_reset (base)) {
+      gst_video_encoder_finish_frame (venc, frame);
       return GST_FLOW_ERROR;
+    }
   }
 
   ret = gst_va_base_enc_import_input_buffer (base,
